b38437b6041e21f4b8801eafd6ba4a5094ae87d8
Matthew Wire (MJW Consulting) <mjw@mjwconsult.co.uk>
2019-04-28 18:50:57 +0100

    Modify how we add select elements to form

--- CRM/Haystack/Form/SettingsCustom.php
+++ CRM/Haystack/Form/SettingsCustom.php
@@ -16,22 +16,20 @@ class CRM_Haystack_Form_SettingsCustom extends CRM_Haystack_Form_Settings {
   public static function addSelectElement(&$form, $name, $setting) {
     switch ($name) {
       case 'theme':
-        $form->addSelect($name,
-          [
-            'options' => ['Haystack' => 'Haystack - based on CiviCRM Admin Utilities', 'Shelford' => 'Shelford - targetted to match Joomla admin theme'],
-            'label' => $setting['description'],
-            'placeholder'  => NULL,
-          ]
+        $form->add(
+          'select',
+          $name,
+          $setting['description'],
+          ['Haystack' => 'Haystack - based on CiviCRM Admin Utilities', 'Shelford' => 'Shelford - targetted to match Joomla admin theme']
         );
         break;
 
       case 'theme_frontend':
-        $form->addSelect($name,
-          [
-            'options' => [0 => 'Never', 1 => 'Only on Frontend (Public pages)', 2 => 'Only on Backend (Admin pages)', 3 => 'Always'],
-            'label' => $setting['description'],
-            'placeholder'  => NULL,
-          ]
+        $form->add(
+          'select',
+          $name,
+          $setting['description'],
+          [0 => 'Never', 1 => 'Only on Frontend (Public pages)', 2 => 'Only on Backend (Admin pages)', 3 => 'Always']
         );
         break;
 
